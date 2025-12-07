bea71a8d64e4235e57592f434e7e48930c9045aa
Seunghyun Hwang <lesomnus@gmail.com>
2025-12-07 11:41:16 +0000

    add Err, ErrIf, PanicIf
    Signed-off-by: Seunghyun Hwang <lesomnus@gmail.com>

--- error.go
+++ error.go
@@ -25,3 +25,29 @@ func CatErr(msg string, err error) error {
 
 	return fmt.Errorf("%s%w", msg, err)
 }
+
+// Err returns a formatted error wrapping `err`.
+//
+//	fmt.Errorf(format+": %w", args..., err)
+func Err(err error, format string, args ...any) error {
+	msg := fmt.Sprintf(format, args...)
+	return fmt.Errorf("%s: %w", msg, err)
+}
+
+// ErrIf returns nil if `err` is nil.
+// Otherwise it returns:
+//
+//	fmt.Errorf(format+": %w", args..., err)
+func ErrIf(err error, format string, args ...any) error {
+	if err == nil {
+		return nil
+	}
+	return Err(err, format, args...)
+}
+
+// PanicIf panics with a formatted message if `err` is not nil.
+func PanicIf(err error, msg string, args ...any) {
+	if err != nil {
+		panic(fmt.Errorf(msg+": %w", append(args, err)...))
+	}
+}
--- error_test.go
+++ error_test.go
@@ -31,3 +31,53 @@ func TestCatErr(t *testing.T) {
 		require.Equal(t, "ab", err.Error())
 	})
 }
+
+func TestErr(t *testing.T) {
+	t.Run("without args", func(t *testing.T) {
+		err := z.Err(errors.New("b"), "a")
+		require.Error(t, err)
+		require.Equal(t, "a: b", err.Error())
+	})
+	t.Run("with args", func(t *testing.T) {
+		err := z.Err(errors.New("b"), "a %d", 42)
+		require.Error(t, err)
+		require.Equal(t, "a 42: b", err.Error())
+	})
+	t.Run("less args", func(t *testing.T) {
+		format := "a %d %d"
+		err := z.Err(errors.New("b"), format, 42)
+		require.Error(t, err)
+		require.Equal(t, "a 42 %!d(MISSING): b", err.Error())
+	})
+	t.Run("more args", func(t *testing.T) {
+		format := "a %d"
+		err := z.Err(errors.New("b"), format, 42, 36)
+		require.Error(t, err)
+		require.Equal(t, "a 42%!(EXTRA int=36): b", err.Error())
+	})
+}
+
+func TestErrIf(t *testing.T) {
+	t.Run("nil error", func(t *testing.T) {
+		err := z.ErrIf(nil, "a %d", 42)
+		require.NoError(t, err)
+	})
+	t.Run("non-nil error", func(t *testing.T) {
+		err := z.ErrIf(errors.New("b"), "a %d", 42)
+		require.Error(t, err)
+		require.Equal(t, "a 42: b", err.Error())
+	})
+}
+
+func TestPanicIf(t *testing.T) {
+	t.Run("nil error", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			z.PanicIf(nil, "a %d", 42)
+		})
+	})
+	t.Run("non-nil error", func(t *testing.T) {
+		require.Panics(t, func() {
+			z.PanicIf(errors.New("b"), "a %d", 42)
+		})
+	})
+}
