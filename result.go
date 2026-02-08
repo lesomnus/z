5dfff4d86370bcf3ccd3ebac2bfa4f963451191a
Seunghyun Hwang <lesomnus@gmail.com>
2026-02-08 22:58:36 +0900

    add utilities for Result
    Signed-off-by: Seunghyun Hwang <lesomnus@gmail.com>

--- result.go
+++ result.go
@@ -1,18 +1,46 @@
 package z
 
 type Result[T any] struct {
-	Value T
+	Value *T
 	Error error
 }
 
 func Take[T any](v T, err error) Result[T] {
-	return Result[T]{Value: v, Error: err}
+	return Result[T]{Value: &v, Error: err}
 }
 
 func (r Result[T]) To(p *T) error {
 	if r.Error != nil {
 		return r.Error
 	}
-	*p = r.Value
+	if r.Value != nil {
+		*p = *r.Value
+	}
 	return nil
+}
+
+func (r Result[T]) IsPending() bool {
+	return r.Value == nil && r.Error == nil
+}
+
+func (r Result[T]) Spread() (T, error) {
+	var z T
+	if r.Error != nil {
+		return z, r.Error
+	}
+	if r.Value == nil {
+		return z, nil
+	}
+	return *r.Value, nil
+}
+
+func (r Result[T]) Must() T {
+	if r.Error != nil {
+		panic(r.Error)
+	}
+	if r.Value == nil {
+		panic("result is pending")
+	}
+
+	return *r.Value
 }
--- result_test.go
+++ result_test.go
@@ -25,4 +25,20 @@ func TestResult(t *testing.T) {
 		require.ErrorIs(t, err, io.EOF)
 		require.Equal(t, 0, v)
 	})
+	t.Run("IsPending", func(t *testing.T) {
+		r := z.Result[int]{}
+		require.True(t, r.IsPending())
+
+		r = z.Take(42, nil)
+		require.False(t, r.IsPending())
+
+		r = z.Take(0, io.EOF)
+		require.False(t, r.IsPending())
+	})
+	t.Run("Spread", func(t *testing.T) {
+		r := z.Take(42, nil)
+		v, err := r.Spread()
+		require.NoError(t, err)
+		require.Equal(t, 42, v)
+	})
 }
